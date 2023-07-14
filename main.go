612a4c799d48b785a1e4f5e19d13ba580de3f7f8
RageNeko26 <rneko2006@gmail.com>
2023-07-14 23:43:23 +0700

    fix: add some information about Ubuntu version

--- main.go
+++ main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"os/exec"
+	"strings"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	getOS := exec.Command("bash", "-c", "cat /etc/os-release | grep 'UBUNTU_CODENAME' ")
+	getOS := []string{
+		"cat /etc/os-release | grep 'PRETTY_NAME'",
 
-	out, err := getOS.Output()
-
-	if err != nil {
-		color.Red("Erro: %d", err)
+		"cat /etc/os-release | grep 'UBUNTU_CODENAME'",
 	}
 
-	color.Yellow(string(out))
+	border := strings.Repeat("#", 30)
+	color.Green(border)
 
+	for _, cmd := range getOS {
+		c := exec.Command("bash", "-c", cmd)
+		out, err := c.Output()
+
+		if err != nil {
+			color.Red("Error: %d", err)
+		}
+
+		color.Yellow(string(out))
+	}
+
+	color.Green(border)
 }
